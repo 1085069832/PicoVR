bd94095f55677f2751b16c036951f4e6747651db
1085069832 <1085069832@qq.com>
2017-07-04 16:43:49 +0800

    按钮设置位置

--- Assets/LeapMotion/DemoResources/Scripts/FlowerBloom.cs
+++ Assets/LeapMotion/DemoResources/Scripts/FlowerBloom.cs
@@ -10,34 +10,37 @@ using System.Collections;
 /// <summary>
 /// 展开花瓣
 /// </summary>
-public class FlowerBloom : MonoBehaviour {
+public class FlowerBloom : MonoBehaviour
+{
 
-  public AnimationCurve openCurve;
-  public float openAngle = -47;
-  public float closeAngle = 100;
-  public float openTime = 1.0f;
-  public float closeTime = 1.0f;
-  public bool open = false;
+    public AnimationCurve openCurve;
+    public float openAngle = -47;
+    public float closeAngle = 100;
+    public float openTime = 1.0f;
+    public float closeTime = 1.0f;
+    public bool open = false;
 
-  public HingeJoint[] pedals;
+    public HingeJoint[] pedals;
 
-  private float phase_ = 1.0f;
+    public float phase_ = 1.0f;
 
-  void Update() {
-    if (open)//开启花瓣
-      phase_ += Time.deltaTime / openTime;
-    else
-      phase_ -= Time.deltaTime / closeTime;
+    void Update()
+    {
+        if (open)//开启花瓣
+            phase_ += Time.deltaTime / openTime;
+        else
+            phase_ -= Time.deltaTime / closeTime;
 
-    phase_ = Mathf.Clamp(phase_, 0.0f, 1.0f);
+        phase_ = Mathf.Clamp(phase_, 0.0f, 1.0f);
 
-    float percent_done = openCurve.Evaluate(phase_);
-    float angle = closeAngle + percent_done * (openAngle - closeAngle);
+        float percent_done = openCurve.Evaluate(phase_);
+        float angle = closeAngle + percent_done * (openAngle - closeAngle);
 
-    foreach (HingeJoint hinge in pedals) {
-      JointSpring spring = hinge.spring;//弹簧
-      spring.targetPosition = angle;//设置弹簧拉向的角度
-      hinge.spring = spring;
+        foreach (HingeJoint hinge in pedals)
+        {
+            JointSpring spring = hinge.spring;//弹簧
+            spring.targetPosition = angle;//设置弹簧拉向的角度
+            hinge.spring = spring;
+        }
     }
-  }
 }
---
+++ Assets/Scripts/UI/DisToFlower.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisToFlower : MonoBehaviour
+{
+
+    [SerializeField] GameObject picoCamera;//摄像机
+    [SerializeField] GameObject flowerCenter;//花
+    Vector3 disOffset;//距离向量
+    [SerializeField] float speed = 2f;//速度
+    [SerializeField] float minDis = 0.3f;//最近距离
+    [SerializeField] float maxDis = 2f;//最远距离
+    [SerializeField] float rotateSpeed = 200;//旋转速度
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        disOffset = picoCamera.transform.position - flowerCenter.transform.position;
+    }
+    /// <summary>
+    /// 拉近
+    /// </summary>
+    public void OnClickToNear()
+    {
+        if (disOffset.magnitude < minDis)
+        {
+            return;
+        }
+        picoCamera.transform.position -= speed * Time.deltaTime * disOffset.normalized;
+    }
+    /// <summary>
+    /// 拉远
+    /// </summary>
+    public void OnClickToKeep()
+    {
+        if (disOffset.magnitude > maxDis)
+        {
+            return;
+        }
+        picoCamera.transform.position += speed * Time.deltaTime * disOffset.normalized;
+    }
+    /// <summary>
+    /// 旋转
+    /// </summary>
+    public void OnClickToRotate()
+    {
+        print("旋转");
+        picoCamera.transform.RotateAround(flowerCenter.transform.position, flowerCenter.transform.up, rotateSpeed * Time.deltaTime);
+    }
+}
